5be5509b51be3c2046ce5b1b9f67202ed7506056
Esa Halsti <esa.halsti@gmail.com>
2015-12-26 23:18:12 +0200

    refactoring of private static fetchData(), private static updateDaily(), private static fetchAllDataFromPrh()

--- app/Company.php
+++ app/Company.php
@@ -6,7 +6,47 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    private static $URL = 'http://avoindata.prh.fi:80/tr/v1?totalResults=true&maxResults=1000&resultsFrom='
+    private static $URL = 'http://avoindata.prh.fi:80/tr/v1?totalResults=true&maxResults=1000&resultsFrom=';
+
+    private static fetchData($URL)
+    {
+        $url = $URL;
+        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+        $result=curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
+    }
+
+    /*
+     * $data = [
+     *    int 'business_id',
+     *    string 'name'
+     *    string 'companyForm',
+     *    date 'registrationDate'
+     * ]
+     */
+    private static saveData($data)
+    {
+        // find company by business_id
+        $company = Company::where('business_id', $data->businessId)->first();
+        // if company not found -> create
+        if ( is_null($company)) {
+            $company = Company::create([
+                'business_id' => $data->businessId,
+                'name' => $data->name,
+                'company_form' => $data->companyForm,
+                'registration_date' => $data->registrationDate,
+            ]);
+
+            $addedCompanies[] = $company->businessId;
+
+        }
+    }
 
     protected $fillable = [
         'business_id',
@@ -33,75 +73,25 @@ class Company extends Model
     /*
      * This function is used to get daily updates from the PRH api
      */
-    public static  function updateDaily()
+    public static function updateDaily()
     {
         $url = self::$URL . 'companyRegistrationFrom=2015-11-16&companyChangedSince=2015-11-16';
-        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-        $result=curl_exec($ch);
-        curl_close($ch);
-        $resultObject = json_decode($result);
-        foreach ($resultObject->results as $co) {
-            // find company by business_id
-            $company = Company::where('business_id', $co->businessId)->first();
-            // if company not found -> create
-            if ( is_null($company)) {
-                $company = Company::create([
-                    'business_id' => $co->businessId,
-                    'name' => $co->name,
-                    'company_form' => $co->companyForm,
-                    'registration_date' => $co->registrationDate,
-                ]);
-
-                $addedCompanies[] = $company->businessId;
-
-            }
-
-        }
-
+        $data = $self::fetchData($url);
+        self::saveData($data);
+        return $data;
     }
 
+    /*
+     * Get the initial data for database
+     */
     public static function fetchAllDataFromPrh($start = 0, $end = 350000)
     {
-        //init added companies array
-        $addedCompanies = [];
+        $url = self::$URL . $i . '&companyRegistrationFrom=1800-01-01';
         for ($i = $start; $i < $end ; $i = $i + 1000) {
-            $url = self::$URL . $i . '&companyRegistrationFrom=1800-01-01';
-            $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_URL,$url);
-            curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-            $result=curl_exec($ch);
-            curl_close($ch);
-            $resultObject = json_decode($result);
-            foreach ($resultObject->results as $co) {
-                // find company by business_id
-                $company = Company::where('business_id', $co->businessId)->first();
-                // if company not found -> create
-                if ( is_null($company)) {
-                    $company = Company::create([
-                        'business_id' => $co->businessId,
-                        'name' => $co->name,
-                        'company_form' => $co->companyForm,
-                        'registration_date' => $co->registrationDate,
-                    ]);
-
-                    $addedCompanies[] = $company->businessId;
-
-                }
-
-            }
-
+            $data = self::fetchData($url);
+            self::saveData($data);
+            $whole_data[] .= $data;
         }
-        return true;
-
+        return $whole_data;
     }
-
 }
--- readme.md
+++
@@ -1,6 +0,0 @@
-# comex
-- Use at your own risk
-- We do not provide any warranty or help
-
-
-All rights reserved.
